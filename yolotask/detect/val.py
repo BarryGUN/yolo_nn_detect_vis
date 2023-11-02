c9125284b9e0e840fea6d6b960e572d118d7afd5
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2023-11-02 11:24:39 +0800

    fix

--- yolotask/detect/val.py
+++ yolotask/detect/val.py
@@ -29,8 +29,6 @@ import numpy as np
 import torch
 from tqdm import tqdm
 
-from models.blocks import FastRepVGGBlockV2, FastRepDBlock, FastRepVGGBlockV3
-
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
