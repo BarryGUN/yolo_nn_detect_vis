f6dfb99d99ef4ab92d2d8e630dc1e8b8efcaed23
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2024-04-10 09:26:41 +0800

    SCWDLoss

--- utils/loss/loss_utils.py
+++ utils/loss/loss_utils.py
@@ -184,10 +184,11 @@ class MimicLoss(nn.Module):
 
 
 class SCWDLoss(nn.Module):
-    def __init__(self, tau=1.0):
+    def __init__(self, tau=1.0, c_gain=0.75, pix_gain=0.25):
         super(SCWDLoss, self).__init__()
         self.tau = tau
-
+        self.c_gain = c_gain
+        self.pix_gain = pix_gain
 
     def forward(self, y_s, y_t):
         """Forward computation.
@@ -206,26 +207,27 @@ class SCWDLoss(nn.Module):
             assert s.shape == t.shape
             N, C, H, W = s.shape
             # normalize in channel diemension
-            # channel wise
             t = t.view(-1, W * H)
             s = s.view(-1, W * H)
+
+            # channel wise
             softmax_pred_T = F.softmax(t / self.tau,
                                        dim=1)  # [N*C, H*W]
-
-            # spatial wise
-            softmax_pred_T_gain = F.softmax(t / self.tau,
-                                       dim=0)  # [N*C, H*W]
-            softmax_pred_S_gain = F.softmax(s / self.tau,
-                                         dim=0)
-
             logsoftmax = torch.nn.LogSoftmax(dim=1)
-
-            cost = torch.sum(
-                softmax_pred_T * logsoftmax(t * softmax_pred_T_gain / self.tau) -
-                 softmax_pred_T * logsoftmax(s * softmax_pred_S_gain / self.tau) ) * (
+            cost = self.c_gain * torch.sum(
+                softmax_pred_T * logsoftmax(t / self.tau) -
+                softmax_pred_T * logsoftmax(s / self.tau)) * (
                            self.tau ** 2)
 
-            losses.append(cost/(C*N))
+            # spatial wise
+            softmax_pred_T = F.softmax(t / self.tau,
+                                       dim=0)  # [N*C, H*W]
+            logsoftmax = torch.nn.LogSoftmax(dim=0)
+            cost += self.pix_gain * torch.sum(
+                softmax_pred_T * logsoftmax(t / self.tau) -
+                softmax_pred_T * logsoftmax(s / self.tau)) * (
+                            self.tau ** 2)
+
+            losses.append(cost / (C * N * 2))
 
         return sum(losses)
-
