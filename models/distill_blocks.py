143cc9f4ccfe6ae25fce7c1f3d5b874279ecc2aa
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2024-04-14 10:27:56 +0800

    SCWDLoss

--- models/distill_blocks.py
+++ models/distill_blocks.py
@@ -12,7 +12,7 @@ class TranROI(nn.Module):
 
     def __init__(self, dim):
         super(TranROI, self).__init__()
-        self.a = Conv(dim, dim, k=3, s=1)
+        self.a = DWConv(dim, dim, k=3, s=1)
         self.v = nn.Identity()
         self.linear = Conv(dim, dim, k=1, s=1, act=False)
 
@@ -25,7 +25,7 @@ class MatROI(nn.Module):
 
     def __init__(self, dim):
         super(MatROI, self).__init__()
-        self.a = Conv(dim, dim, k=3, s=1)
+        self.a = DWConv(dim, dim, k=3, s=1)
         self.v = nn.Identity()
         self.linear = Conv(dim, dim, k=1, s=1, act=False)
 
