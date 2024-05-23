d63d45a66a8ec69e9b346be784858282fe07acae
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2024-05-23 09:17:58 +0800

    SCWDLoss

---
+++ configs/train/yolonn-n-expfree_siou_640.py
@@ -0,0 +1,21 @@
+config = {
+    'hyp': 'data/hyps/hyp.scratch-vis.yaml',
+    'cfg':  'cfg/yolonn-vis.yaml',
+    'data': 'data/VisDrone.yaml',
+    'amp': True,
+    'batch-size': 16,
+    'weights': '',
+    'project': 'run/train/n',
+    'name': 'yolonn-vis-org-expfree_ciou_640',
+    'model-scale': 'n',
+    'iou': 'SIoU',
+    'detector': 'ExpFree',
+    'optimizer': 'SGD',
+    'epochs': 600,
+    'cos-lr': True,
+    'low-gpu-mem': True,
+    'imgsz': 640,
+
+
+
+}
