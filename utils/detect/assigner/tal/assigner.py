4c1c5ffd7fa790290f847ccbcdfd51197261ff97
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2024-05-23 11:41:13 +0800

    SCWDLoss

--- utils/detect/assigner/tal/assigner.py
+++ utils/detect/assigner/tal/assigner.py
@@ -232,8 +232,8 @@ class ExpFreeTaskAlignedAssigner(nn.Module):
 
     # 1.0
     # 0.095
-    # 2
-    def __init__(self, topk=13, num_classes=80, alpha=1.0, beta=2.0, eps=1e-9):
+    # 2.0
+    def __init__(self, topk=13, num_classes=80, alpha=1.0, beta=6.0, eps=1e-9):
         super().__init__()
         self.topk = topk
         self.num_classes = num_classes
