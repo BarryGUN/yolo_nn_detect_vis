17c255780baedd89262dad9ee3820984a32e47be
BarryGUN <10863370+barryGUN@user.noreply.gitee.com>
2024-05-24 11:26:21 +0800

    SCWDLoss

--- align_visualize.py
+++ align_visualize.py
@@ -6,10 +6,10 @@ from matplotlib import pyplot as plt
 # 0.05
 # 0.075
 # 2
-def align_new(x, y, alpha=0.5, beta=0.05):
+def align_new(x, y, alpha=0.5, beta=0.25):
     # return pow(x, alpha) * abs(beta * y / ((beta + 1) - y))
     # return pow(x, alpha) * (beta * y / ((beta + 1) - y))
-    return pow(x, alpha) * (beta * abs(y)/ ((beta + 1) - abs(y)))
+    return pow(x, alpha) * abs(beta * abs(y)/ ((beta + 1) - abs(y)))
 
 # # beta=0.095
 # # beta =1
@@ -47,6 +47,32 @@ def scatter_plot(npz, fcns, topk):
         plt.show()
 
 
+def scatter_plot_3d(npz, fcns, topk):
+    loaded_data = np.load(npz)
+    xys = (loaded_data['xs'], loaded_data['ys'])
+    matrics = {}
+    for fcn in fcns:
+        results = []
+        for i, (x, y) in enumerate(zip(xys[0], xys[1])):
+            results.append((i, x, y, fcn(x, y)))
+        matrics[fcn.__name__] = sorted(results, key=lambda v: v[-1], reverse=True)[:topk]
+
+
+    for key in matrics.keys():
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(xys[0], xys[1], eval(f'{key}(xys[0],xys[1])'), c='blue', s=2)
+        xk = [e[1] for e in matrics[key]]
+        yk = [e[2] for e in matrics[key]]
+        zk = [e[3] for e in matrics[key]]
+        ax.scatter(xk, yk, zk, c='red', s=2)
+        # ax.set_legend(loc='upper left')
+        ax.set_title(f'{key}')
+        ax.set_xlabel('bbox-score')
+        ax.set_ylabel('overlaps')
+        ax.set_zlabel('score')
+        plt.show()
+
 
 
 if __name__ == '__main__':
@@ -56,4 +82,5 @@ if __name__ == '__main__':
     # np.savez('data_3.npz', xs=np.random.rand(8400), ys=np.random.uniform(-1, 1, 8400))
     fcns = [align_new, align_org]
     # # # # fcns = [ align_org]
-    scatter_plot('data_3.npz',fcns=fcns, topk=10)
+    # scatter_plot('data_3.npz',fcns=fcns, topk=10)
+    scatter_plot_3d('data_3.npz', fcns=fcns, topk=10)
